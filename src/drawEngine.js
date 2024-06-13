c9b3cf2a08ff18313ec080173d8098ef9843015f
ust-Eseniya <162564390+ust-Eseniya@users.noreply.github.com>
2024-06-13 10:18:41 +0500

    Update drawEngine.js

--- src/drawEngine.js
+++ src/drawEngine.js
@@ -1,21 +1,43 @@
 class DrawEngine {
-    drawImage ({ spruteSheet, image, x, y, width, height }){}
-    clear(){}
-}
-
-class CanvasDrawEngine extends DrawEngine{
-    constructor ({ canvas }) {
-        super()
-        this._canvas = canvas
-        this._context = canvas.getContext('2d')
-    }
-    drawImage({ spriteSheet, image, x, y, height}) {
-        super.drawImage({ spriteSheet, image, x, y, width, height })
-        this._context.drawImage(spriteSheet, image.x, image.y, image.w, image.y, x, y, width, height)
+    drawImage({ spriteSheet, image, x, y, width, height }) {}
+    clear() {}
+  }
+  
+  class CanvasDrawEngine extends DrawEngine {
+    constructor({ canvas, game }) {
+      super();
+      this._canvas = canvas;
+      this._context = canvas.getContext("2d");
+      this._game = game;
     }
 
-    clear(){
-        super.clear()
-        this._context.clearRect(0, 0, this._canvas.width, this._canvas.height)
+    drawImage({ spriteSheet, image, x, y, width, height }) {
+      super.drawImage({ spriteSheet, image, x, y, width, height });
+      this._context.drawImage(
+        spriteSheet,
+        image.x,
+        image.y,
+        image.w,
+        image.h,
+        x,
+        y,
+        width,
+        height
+      );
     }
-}
+
+    drawText({ x, y, text }) {
+      this._context.fillStyle = "tomato";
+      if (this._game._config.score > 9) {
+        this._context.font = "14px 'Press Start 2P'";
+      } else {
+        this._context.font = "16px 'Press Start 2P'";
+      }
+      this._context.fillText(text, x, y);
+    }
+
+    clear() {
+      super.clear();
+      this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    }
+  }
