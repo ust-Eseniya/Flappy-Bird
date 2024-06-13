7928a366a947c979709160152e5a20c0fb279b07
ust-Eseniya <162564390+ust-Eseniya@users.noreply.github.com>
2024-06-13 10:21:17 +0500

    physicsEngines.js

--- src/phisicsEngines.js
+++ src/phisicsEngines.js
@@ -1,11 +1,12 @@
 class PhysicsEngine {
-    constructor ({ gravity }){
-        this._gravity = gravity
+    constructor({ gravity }) {
+      this._gravity = gravity;
     }
+  
     update(entity, delta) {
-        if (entity.falling){
-            entity.speed += this._gravity * delta
-            entity.y += entity.speed * delta
-        }
+      if (entity.falling) {
+        entity.speed += this._gravity * delta;
+        entity.y += entity.speed * delta;
+      }
     }
-}
+  }
