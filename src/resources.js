fc491e29c9a58e68fc429341e60ef8404198aab1
ust-Eseniya <162564390+ust-Eseniya@users.noreply.github.com>
2024-06-13 10:21:52 +0500

    Update resources.js

--- src/resources.js
+++ src/resources.js
@@ -1,22 +1,20 @@
-const RESOURCE_TYPE = {
-    IMAGE: 'image',
-}
-
-class ResourceLoader{
+const RESOURSE_TYPE = {
+    IMAGE: "image",
+  };
+  
+  class ResourceLoader {
     _typeLoadersMap = {
-        [RESOURCE_TYPE.IMAGE]: async ({ src, width, height}) => {
-            return new Promise((resolve, reject) => {
-            const image = new Image(width, height)
-            image.addEventListener('load', () => resolve(image))
-            image.addEventListener('error', (error) => reject(error))
-            image.src = src
-            })
-        }
+      [RESOURSE_TYPE.IMAGE]: async ({ src, width, height }) => {
+        return new Promise((resolve, reject) => {
+          const image = new Image(width, height);
+          image.addEventListener("load", () => resolve(image));
+          image.addEventListener("error", (error) => reject(error));
+          image.src = src;
+        });
+      },
+    };
+    async load(resource) {
+      const loader = this._typeLoadersMap[resource.type];
+      return await loader(resource);
     }
-
-    async load(resource){
-        const loader = this._typeLoadersMap[resource.type]
-        return await loader(resource)
-    }
-}
-
+  }
