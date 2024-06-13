85f9fa28de2cd44998ff5afe3d744fe62999f758
ust-Eseniya <162564390+ust-Eseniya@users.noreply.github.com>
2024-06-13 10:20:22 +0500

    Update inputHandlers.js

--- src/inputHandlers.js
+++ src/inputHandlers.js
@@ -1,30 +1,50 @@
-class inputHandler {
-    eventHandlerMap = {}
-
-    constructor(eventHandlerConfig){
-        this._eventHandlerConfig = eventHandlerConfig
+class InputHandler {
+    eventHandlerMap = {};
+  
+    constructor(eventHandlerConfig) {
+      this._eventHandlerConfig = eventHandlerConfig;
     }
-    subscribe(){
-        Object.entries(this.eventHandlerMap).forEach(([name, hander]) => {
-            document.addEventListener(name, hamdler)
-        })
+    subscribe() {
+      Object.entries(this.eventHandlerMap).forEach(([name, handler]) => {
+        document.addEventListener(name, handler);
+      });
     }
-}
-
-class MouseInputHandler extends inputHandler {
-    buttonIndexNameMap = {
-        0: 'left',
-        1: 'middle',
-        2: 'right',
-    }
-
+  }
+  
+  class MouseInputHandler extends InputHandler {
+    buttomIndexNameMap = {
+      0: "left",
+      1: "middle",
+      2: "right",
+    };
+  
     eventHandlerMap = {
-        click: (event) => {
-            const buttonName = this.buttonIndexNameMap[event.button]
-            const handler = this._eventHandlerConfig[buttonName]
-            if (handler){
-                handler(event)
-            }
+      click: (event) => {
+        const buttonName = this.buttomIndexNameMap[event.button];
+        const handler = this._eventHandlerConfig[buttonName];
+        if (handler) {
+          handler(event);
         }
-    }
-}
+      },
+    };
+  }
+  
+  class KeyboardInputHandler extends InputHandler {
+    buttomIndexNameMap = {
+      ArrowUp: "ArrowUp",
+      Space: "Space",
+      KeyW: "KeyW",
+      KeyC: "KeyC",
+      KeyR: "KeyR",
+    };
+  
+    eventHandlerMap = {
+      keydown: (event) => {
+        const buttonName = this.buttomIndexNameMap[event.code];
+        const handler = this._eventHandlerConfig[buttonName];
+        if (handler) {
+          handler(event);
+        }
+      },
+    };
+  }
