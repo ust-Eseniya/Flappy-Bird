5da35586248f6ce8c8710bf232935fac7cb53f56
ust-Eseniya <162564390+ust-Eseniya@users.noreply.github.com>
2024-06-13 10:23:24 +0500

    Update base.js

--- src/entities/base.js
+++ src/entities/base.js
@@ -1,31 +1,20 @@
 class Entity {
-    constructor({ x, y, width, height, frames, spriteSheet, drawEngine, game }){
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.speed = 0 
-        this.fallind = false
+  constructor({ x, y, width, height, frames, spriteSheet, drawEngine, game }) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = 0;
+    this.falling = false;
 
-        this._frames = frames
-        this._frameIdx = 0
-        this._spriteSheet = spriteSheet
-        this._drawEngine = drawEngine
-        this._game = game
-    }
+    this._frames = frames;
+    this._frameIdx = 0;
+    this._spriteSheet = spriteSheet;
+    this._drawEngine = drawEngine;
+    this._game = game;
+  }
 
-    draw() {
-        this._drawEngine.drawImage({
-          spriteSheet: this._spriteSheet, 
-          image: this._frames[this._frameIdx], 
-          x: this.x, 
-          y: this.y, 
-          width: this.width, 
-          height: this.height,
-        })
-    }
-
-    update(delta) {
-        this._frameIdx = (this._frameIdx + Math.ceil(delta)) % this._frames.length
-      }
+  update(delta) {
+    this._frameIdx = (this._frameIdx + Math.ceil(delta)) % this._frames.length;
+  }
 }
