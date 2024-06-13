81976220cad57942e22c7bbff89ca0336311dbc4
ust-Eseniya <162564390+ust-Eseniya@users.noreply.github.com>
2024-06-13 10:22:54 +0500

    Create sounds.js

---
+++ src/sounds.js
@@ -0,0 +1,14 @@
+const flapSound = new Audio();
+flapSound.src = "./assets/audio/flap.mp3";
+
+const continueSound = new Audio();
+continueSound.src = "./assets/audio/aaaa.mp3";
+
+const dieSound = new Audio();
+dieSound.src = "./assets/audio/die.mp3";
+
+const hitSound = new Audio();
+hitSound.src = "./assets/audio/hit.mp3";
+
+const pointSound = new Audio();
+pointSound.src = "./assets/audio/point.mp3";
